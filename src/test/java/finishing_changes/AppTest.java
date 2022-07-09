ce7e8cc4c22671b491a2948d0d0d5500add122a7
eswarisai <brazilregression@gmail.com>
2022-07-09 23:10:37 +0530

    changes done by whitegovinda

---
+++ src/test/java/finishing_changes/AppTest.java
@@ -0,0 +1,20 @@
+package finishing_changes;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
